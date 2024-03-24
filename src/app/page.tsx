4f5c8da270a0b9d8f5b381bd221e65dbb9d90433
poylib <poy_lib@naver.com>
2024-03-24 17:58:23 +0900

    Test: change git config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>/dev</main>;
+  return (
+    <main className={styles.main}>
+      <p>config test</p>
+    </main>
+  );
 }
