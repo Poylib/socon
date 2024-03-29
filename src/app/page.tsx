1824383c544edb43ec3b36c458a6b1adfaa65ea5
poylib <poy_lib@naver.com>
2024-03-29 20:18:06 +0900

    Test: deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <p>config test</p>
-    </main>
-  );
+  return <main className={styles.main}>test</main>;
 }
