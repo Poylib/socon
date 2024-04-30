b760af301618690c8a830f7cac913136581ea054
Poylib <poy_lib@naver.com>
2024-04-30 19:00:47 +0900

    Fix: build error

--- src/app/components/main/Carousel.tsx
+++ src/app/components/main/Carousel.tsx
@@ -3,12 +3,10 @@ import Image from "next/image";
 import main_1 from "@@/main/main_1.png";
 import main_2 from "@@/main/main_2.png";
 
-import styles from "./Carousel.module.scss";
-
 export default function Carousel() {
   return (
-    <div className="snap-y snap-mandatory h-screen w-screen overflow-scroll">
-      <section className="relative flex items-center justify-center h-screen w-full overflow-hidden snap-always snap-center">
+    <div className="h-[500vh] overflow-hidden">
+      <section className="relative flex items-center justify-center h-screen w-screen overflow-hidden">
         <Image //
           fill={true}
           src={main_1}
@@ -17,15 +15,42 @@ export default function Carousel() {
           placeholder="blur"
         />
       </section>
-      {/* <section className="relative flex items-center justify-center h-screen w-full overflow-hidden snap-always snap-center">
+      <section className="relative flex items-center justify-center h-screen w-screen overflow-hidden">
         <Image //
           fill={true}
-          src={main_2}
+          src={main_1}
           style={{ objectFit: "cover" }}
           alt="메인사진2"
           placeholder="blur"
         />
-      </section> */}
+      </section>
+      <section className="relative flex items-center justify-center h-screen w-screen overflow-hidden">
+        <Image //
+          fill={true}
+          src={main_1}
+          style={{ objectFit: "cover" }}
+          alt="메인사진2"
+          placeholder="blur"
+        />
+      </section>
+      <section className="relative flex items-center justify-center h-screen w-screen overflow-hidden">
+        <Image //
+          fill={true}
+          src={main_1}
+          style={{ objectFit: "cover" }}
+          alt="메인사진2"
+          placeholder="blur"
+        />
+      </section>
+      <section className="relative flex items-center justify-center h-screen w-screen overflow-hidden">
+        <Image //
+          fill={true}
+          src={main_1}
+          style={{ objectFit: "cover" }}
+          alt="메인사진2"
+          placeholder="blur"
+        />
+      </section>
     </div>
   );
 }
