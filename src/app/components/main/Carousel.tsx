8c96ae46c8dea56377a9ec4938b4da52d520d131
Poylib <poy_lib@naver.com>
2024-05-02 17:00:51 +0900

    Feature: scroll test

--- src/app/components/main/Carousel.tsx
+++ src/app/components/main/Carousel.tsx
@@ -5,46 +5,85 @@ import { useEffect, useRef } from "react";
 import main_1 from "@@/main/main_1.png";
 import main_2 from "@@/main/main_2.png";
 import useMainAnimation from "@/store/useMainAnimation";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 export default function Carousel() {
   const { setCurrentBg } = useMainAnimation();
   let containerRef = useRef(null);
+  const main = useRef();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      gsap.registerPlugin(ScrollTrigger);
-    }
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: "+=200 70%",
-        end: "+=200 60%",
-        scrub: true,
-        markers: true,
-        pinSpacing: false,
-        onEnter: () => {
-          setCurrentBg("#fff");
-          // gsap.to(textRef.current, {
-          //   color: "#ffca28",
-          //   duration: 1,
-          // });
-        },
-        onLeaveBack: () => {
-          setCurrentBg("#222");
-          // gsap.to(textRef.current, {
-          //   color: "#000",
-          //   duration: 1,
-          // });
-        },
-      },
-    });
-  }, []);
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(".box");
+      boxes.forEach((box) => {
+        gsap.to(box, {
+          x: 150,
+          backgroundColor: "red",
+          scrollTrigger: {
+            trigger: box,
+            pin: true,
+            start: "bottom bottom",
+            end: "top 20%",
+            scrub: true,
+            // markers: true,
+          },
+        });
+      });
+    },
+    { scope: main }
+  );
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     gsap.registerPlugin(ScrollTrigger);
+  //   }
+  //   gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: containerRef.current,
+  //       start: "+=200 90%",
+  //       end: "+=200",
+  //       scrub: true,
+  //       markers: true,
+  //       pinSpacing: false,
+  //       onEnter: () => {
+  //         setCurrentBg("#fff");
+  //         // gsap.to(textRef.current, {
+  //         //   color: "#ffca28",
+  //         //   duration: 1,
+  //         // });
+  //       },
+  //       onLeaveBack: () => {
+  //         setCurrentBg("#222");
+  //         // gsap.to(textRef.current, {
+  //         //   color: "#000",
+  //         //   duration: 1,
+  //         // });
+  //       },
+  //     },
+  //   });
+  // }, []);
 
   return (
-    <div className="h-[500vh] overflow-hidden" ref={containerRef}>
+    <div
+      id="container"
+      className="h-[500vh] overflow-hidden"
+      ref={containerRef}
+    >
       <section className="h-screen w-screen">
         <p>blabla</p>
       </section>
+      <div className="section flex-center column" ref={main}>
+        <div className="box" style={{ backgroundColor: "blue" }}>
+          box
+        </div>
+        <div className="box" style={{ backgroundColor: "blue" }}>
+          box
+        </div>
+        <div className="box" style={{ backgroundColor: "blue" }}>
+          box
+        </div>
+      </div>
       <section className="relative flex items-center justify-center h-screen w-screen overflow-hidden">
         <Image //
           fill={true}
