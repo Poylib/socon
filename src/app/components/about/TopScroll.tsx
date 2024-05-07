caf8ada55d4567eb6fb4ec672bf4607ffaa05f9b
Poylib <poy_lib@naver.com>
2024-05-07 15:24:03 +0900

    Feature: about 사진 및 문구 변경

--- src/app/components/about/TopScroll.tsx
+++ src/app/components/about/TopScroll.tsx
@@ -6,13 +6,16 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import Picture1 from "@@/main/top.png";
-import Picture2 from "@@/main/main_2.jpeg";
-import Picture3 from "@@/main/main_3.jpeg";
-import Picture4 from "@@/main/main_4.jpeg";
-import Picture5 from "@@/main/main_5.jpeg";
-import Picture6 from "@@/main/main_6.jpeg";
-import Picture7 from "@@/main/main_6.jpeg";
+import Picture1 from "@@/about/about_2_2.jpeg";
+import Picture1z from "@@/about/about_2_2_z.jpeg";
+import Picture2 from "@@/about/about_1_1.jpeg";
+import Picture3 from "@@/about/about_1_2.jpeg";
+import Picture4 from "@@/about/about_1_3.jpeg";
+import Picture5 from "@@/about/about_2_1.jpg";
+import Picture6 from "@@/about/about_2_3.jpeg";
+import Picture7 from "@@/about/about_3_1.jpeg";
+import Picture8 from "@@/about/about_3_2.jpeg";
+import Picture9 from "@@/about/about_3_3.jpeg";
 
 import styles from "./TopScroll.module.scss";
 
@@ -47,16 +50,20 @@ export default function TopScroll() {
       scale: 5,
     },
     {
-      src: Picture5,
+      src: Picture7,
       scale: 5,
     },
     {
-      src: Picture2,
+      src: Picture8,
       scale: 5,
     },
     {
-      src: Picture3,
+      src: Picture9,
       scale: 5,
+    },
+    {
+      src: Picture1z,
+      scale: 4,
     },
   ];
 
@@ -75,6 +82,7 @@ export default function TopScroll() {
         {
           duration: 1,
           scale: 4,
+          opacity: 1,
         },
         "<"
       );
@@ -117,13 +125,28 @@ export default function TopScroll() {
           );
         })}
         <span
-          className="glowTitle absolute top-[220px] transform translate-y-[40px] opacity-0 text-white text-3xl font-semibold"
+          className="glowTitle absolute top-[80px] transform translate-y-[40px] opacity-0 text-black text-2xl font-semibold"
           style={{ letterSpacing: 5 }}
         >
-          ABOUT
+          So Concretus!
         </span>
-        <span className="glowContent absolute top-[280px] transform translate-y-[20px] opacity-0 text-white">
-          socon이 추구하는 가치. 꿈은 없고 그냥 놀고싶어요
+        <span
+          className="glowContent absolute top-[130px] px-2 transform translate-y-[20px] opacity-0 text-black text-center text-xs"
+          style={{ lineHeight: 1.6 }}
+        >
+          현대 건축의 상징이자 미학의 한 축을 이루는 콘크리트
+          <br />
+          일반적으로 그저 회색의 덩어리 정도로 생각하지만
+          <br />
+          콘크리트의 색상, 형태, 마감 방법에 따라 건축물 전체의 이미지를
+          바꿉니다
+          <br />
+          Concretus는 콘크리트의 어원으로 ‘함께 성장하다’라는 뜻을 지니고
+          있습니다
+          <br />
+          저희 studio SOCON은 클라이언트와 함께 소통하며
+          <br />
+          협업하여 최고의 결과물을 만들어내기 위해 노력합니다
         </span>
       </div>
     </div>
