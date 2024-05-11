3ee2bee3cedf1b78ca9626eba440ae1d2f65075a
poylib <poy_lib@naver.com>
2024-05-11 18:07:25 +0900

    Test: vecel 배포 용량 테스트

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -2,11 +2,12 @@ import { getPosts } from "@/data/jpg/get-contents";
 import PhotoGrid from "../components/category/PhotoGrid";
 
 export default async function Category() {
-  const data = (await getPosts("jpg")).filter((e) => e.thumbnail === "1.jpg");
+  // const data = (await getPosts("jpg")).filter((e) => e.thumbnail === "1.jpg");
 
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-[70px] px-6 w-[100%] max-w-[1024px]">
-      <PhotoGrid data={data} />
+      {/* <PhotoGrid data={data} /> */}
+      <p>test</p>
     </section>
   );
 }
