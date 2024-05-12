fc2b735ec8daf626ff961b839f333ccdc4db38ca
poylib <poy_lib@naver.com>
2024-05-12 17:17:58 +0900

    Chore: remove post folder

--- src/app/components/category/PhotoCard.tsx
+++ src/app/components/category/PhotoCard.tsx
@@ -17,14 +17,14 @@ export default function PhotoCard({ photo, index }: Props) {
     const slug = path.split(" ").join("_");
     router.push(`jpg/${slug}?category=${category}`);
   };
-
+  console.log("bb", photo.src);
   return (
     <div
       className="relative h-[210px] w-[100%] sm:h-[280px] md:h=[360px]"
       onClick={() => goDetail(photo.content, photo.category)}
     >
       <Image
-        src="https://d66us06o4pe3t.cloudfront.net/jpg/[Commercial]_%E1%84%80%E1%85%A5%E1%86%AB%E1%84%8E%E1%85%AE%E1%86%A8%E1%84%80%E1%85%A1%E1%84%92%E1%85%A7%E1%86%B8%E1%84%92%E1%85%AC%20%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%89%E1%85%B5/1.jpg"
+        src={photo.src}
         fill={true}
         style={{ objectFit: "cover" }}
         alt="photo"
