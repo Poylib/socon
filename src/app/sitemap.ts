804e033470643b66fd605671ba65d3e7d8286a3c
poylib <poy_lib@naver.com>
2024-06-02 19:35:51 +0900

    Feature: sitemap 생성

---
+++ src/app/sitemap.ts
@@ -0,0 +1,17 @@
+import { NAV_CATEGORY } from "@/constants/navigation";
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://socon.kr";
+  const tab = NAV_CATEGORY.filter((el) => el.name !== "INSTAGRAM").map((el) => {
+    return { url: `${baseUrl}${el.href}`, lastModified: new Date() };
+  });
+  console.log("🚀 ~ tab ~ tab:", tab);
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    ...tab,
+  ];
+}
