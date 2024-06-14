9b4a1b773bfb13423b8d13b78d304ff199df641a
Poylib <poy_lib@naver.com>
2024-06-14 17:32:05 +0900

    Feature: 영상 썸네일 추가

--- src/app/mp4/page.tsx
+++ src/app/mp4/page.tsx
@@ -5,18 +5,27 @@ export default async function MP4() {
 
   const { Contents } = await getAwsContent(req);
 
+  const mp4Arr = Contents?.filter((el) => el?.Key.includes(".mp4"));
+  const thumbnail = Contents?.filter((el) => el?.Key.includes(".jpeg"));
+  console.log("🪄  MP4  thumbnail", thumbnail);
+
   return (
     <div className="flex flex-col items-center py-[70px] max-w-[1024px]">
-      {Contents?.map((el, idx) => {
-        if (idx)
-          return (
-            <video controls key={idx} className="mb-4">
-              <source
-                src={`https://socon-image.s3.ap-northeast-2.amazonaws.com/${el.Key}`}
-                type="video/mp4"
-              />
-            </video>
-          );
+      {mp4Arr?.map((el, idx) => {
+        return (
+          <video
+            controls
+            key={idx}
+            className="mb-4"
+            style={{ width: idx === 1 ? "50%" : "100%" }}
+            poster={`https://socon-image.s3.ap-northeast-2.amazonaws.com/${thumbnail[idx].Key}`}
+          >
+            <source
+              src={`https://socon-image.s3.ap-northeast-2.amazonaws.com/${el.Key}`}
+              type="video/mp4"
+            />
+          </video>
+        );
       })}
     </div>
   );
