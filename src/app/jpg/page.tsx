6345bbbe6e8d5d71156df09be868603377f9d6cc
poylib <poy_lib@naver.com>
2024-10-30 00:45:27 +0900

    Fix: use VERCEL env
    Fix: url 수정
    
    Fix: url 수정
    
    Fix: url 수정

--- src/app/jpg/page.tsx
+++ src/app/jpg/page.tsx
@@ -1,22 +1,18 @@
 import { JpgResponseType } from "../aws/jpg/api/route";
 import PhotoGrid from "../components/category/PhotoGrid";
 
-async function getCategories(): Promise<JpgResponseType> {
-  const response = await fetch(`${process.env.BASE_URL}/aws/jpg/api`, {
-    cache: "no-store",
-  });
-  return response.json();
-}
+// async function getCategories(): Promise<JpgResponseType> {
+//   const response = await fetch(`https://${process.env.VERCEL_URL}/aws/jpg/api`);
+//   return response.json();
+// }
 
 export default async function Category() {
   // const { data } = await getCategories();
-  const data = "why";
+
   return (
-    <>
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-[70px] px-6 w-[100%] max-w-[1024px]">
-        {/* <PhotoGrid data={data} /> */}
-      </section>
-      <span>{JSON.stringify(data)}</span>
-    </>
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 py-[70px] px-6 w-[100%] max-w-[1024px]">
+      {/* <PhotoGrid data={data} /> */}
+      <span>{process.env.VERCEL_URL}</span>
+    </section>
   );
 }
